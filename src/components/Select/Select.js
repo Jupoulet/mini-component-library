7921da926b548c95211c8380e68f5a6956c911ec
Jupoulet <julien.pierre.picard@gmail.com>
2021-11-05 09:23:38 +0100

    select input

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -4,14 +4,35 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
+import arrow from './arrow.png';
+
+const CustomSelect = styled.select`
+  position: relative;
+  background-color: ${COLORS.transparentGray15};
+  border: none;
+  border-radius: 4px;
+  color: ${COLORS.gray700};
+  padding: 12px 52px 12px 16px;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+
+  background-image: url(${arrow});
+  background-repeat: no-repeat;
+  background-size: 12px;
+  background-position: right 1rem center;
+
+  &:hover {
+    color: black;
+  }
+`
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
+    <CustomSelect value={value} onChange={onChange}>
       {children}
-    </select>
+    </CustomSelect>
   );
 };
 
